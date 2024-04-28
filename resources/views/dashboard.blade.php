1627e7f3b6f5be7b054b0d5e918827802a781b5d
oubahamorad <moradoubaha97@gmail.com>
2024-04-28 20:42:30 +0100

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -172,16 +172,9 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 max-md:max-w-md mx-auto">
                 <div class="bg-white rounded-lg overflow-hidden shadow-lg">
                     <div class="p-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-8 mb-4" viewBox="0 0 32 32">
-                            <path
-                                d="M28.068 12h-.128a.934.934 0 0 1-.864-.6.924.924 0 0 1 .2-1.01l.091-.091a2.938 2.938 0 0 0 0-4.147l-1.511-1.51a2.935 2.935 0 0 0-4.146 0l-.091.091A.956.956 0 0 1 20 4.061v-.129A2.935 2.935 0 0 0 17.068 1h-2.136A2.935 2.935 0 0 0 12 3.932v.129a.956.956 0 0 1-1.614.668l-.086-.091a2.935 2.935 0 0 0-4.146 0l-1.516 1.51a2.938 2.938 0 0 0 0 4.147l.091.091a.935.935 0 0 1 .185 1.035.924.924 0 0 1-.854.579h-.128A2.935 2.935 0 0 0 1 14.932v2.136A2.935 2.935 0 0 0 3.932 20h.128a.934.934 0 0 1 .864.6.924.924 0 0 1-.2 1.01l-.091.091a2.938 2.938 0 0 0 0 4.147l1.51 1.509a2.934 2.934 0 0 0 4.147 0l.091-.091a.936.936 0 0 1 1.035-.185.922.922 0 0 1 .579.853v.129A2.935 2.935 0 0 0 14.932 31h2.136A2.935 2.935 0 0 0 20 28.068v-.129a.956.956 0 0 1 1.614-.668l.091.091a2.935 2.935 0 0 0 4.146 0l1.511-1.509a2.938 2.938 0 0 0 0-4.147l-.091-.091a.935.935 0 0 1-.185-1.035.924.924 0 0 1 .854-.58h.128A2.935 2.935 0 0 0 31 17.068v-2.136A2.935 2.935 0 0 0 28.068 12ZM29 17.068a.933.933 0 0 1-.932.932h-.128a2.956 2.956 0 0 0-2.083 5.028l.09.091a.934.934 0 0 1 0 1.319l-1.511 1.509a.932.932 0 0 1-1.318 0l-.09-.091A2.957 2.957 0 0 0 18 27.939v.129a.933.933 0 0 1-.932.932h-2.136a.933.933 0 0 1-.932-.932v-.129a2.951 2.951 0 0 0-5.028-2.082l-.091.091a.934.934 0 0 1-1.318 0l-1.51-1.509a.934.934 0 0 1 0-1.319l.091-.091A2.956 2.956 0 0 0 4.06 18h-.128A.933.933 0 0 1 3 17.068v-2.136A.933.933 0 0 1 3.932 14h.128a2.956 2.956 0 0 0 2.083-5.028l-.09-.091a.933.933 0 0 1 0-1.318l1.51-1.511a.932.932 0 0 1 1.318 0l.09.091A2.957 2.957 0 0 0 14 4.061v-.129A.933.933 0 0 1 14.932 3h2.136a.933.933 0 0 1 .932.932v.129a2.956 2.956 0 0 0 5.028 2.082l.091-.091a.932.932 0 0 1 1.318 0l1.51 1.511a.933.933 0 0 1 0 1.318l-.091.091A2.956 2.956 0 0 0 27.94 14h.128a.933.933 0 0 1 .932.932Z"
-                                data-original="#000000" />
-                            <path d="M16 9a7 7 0 1 0 7 7 7.008 7.008 0 0 0-7-7Zm0 12a5 5 0 1 1 5-5 5.006 5.006 0 0 1-5 5Z"
-                                data-original="#000000" />
-                        </svg>
-                        <h3 class="text-xl font-semibold mb-2">Customization</h3>
-                        <p class="text-gray-500 text-sm">Tailor our product to suit your needs. Tailor our product to suit
-                            your needs.</p>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="20" class="my-2" width="25" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg>
+                        <h3 class="text-xl font-semibold mb-2">Fast internet</h3>
+                        <p class="text-gray-500 text-sm">refers to high-speed internet connections that provide rapid data transfer rates. This allows users to quickly access and download digital content, stream high-definition videos without buffering, participate in online gaming with minimal lag, and conduct video calls smoothly. Fast internet enables efficient web browsing, faster file downloads and uploads, and seamless online experiences across various devices. It's essential for modern digital activities, from entertainment and communication to work and education..</p>
                     </div>
                 </div>
                 <div class="bg-white rounded-lg overflow-hidden shadow-lg">
@@ -203,7 +196,7 @@
                             </g>
                         </svg>
                         <h3 class="text-xl font-semibold mb-2">Security</h3>
-                        <p class="text-gray-500 text-sm">Your data is protected by the latest security measures.</p>
+                        <p class="text-gray-500 text-sm">Security is a critical aspect of modern digital operations, ensuring the safety and integrity of systems, networks, and data. It encompasses a range of practices and technologies aimed at protecting against unauthorized access, data breaches, cyberattacks, and other security threats.</p>
                     </div>
                 </div>
                 <div class="bg-white rounded-lg overflow-hidden shadow-lg">
@@ -224,7 +217,9 @@
                         <h3 class="text-xl font-semibold mb-2">Performance</h3>
                         <p class="text-gray-500 text-sm">Experience blazing-fast performance with our product.</p>
                     </div>
+                    
                 </div>
+                
             </div>
         </div>
     </div>
